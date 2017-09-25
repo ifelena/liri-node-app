05e06fab5e9c67e56fde54f0cff244c299e58117
Ife Ramsey <ilena130@gmail.com>
2017-09-24 18:10:14 -0600

    liri bot app

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'aCXtH0UiBPVpkXDkXQMeFrMxP',
+  consumer_secret: 'OZMAAQJadhTTF3aKWclh2ICld3JJahR37oQItrsSuHjSC6WS7j',
+  access_token_key: '911070260064698369-9cixgd4A307VP2Dp4kXgfkvIOaveV3Y',
+  access_token_secret: 'U1MucdHA3hzvOSiTBg4Dwni58EfiL2d4jeuoZxp67lK3o',
+}
+
+module.exports = twitterKeys;
